53dc01cc193dd4aca934e4d42a103dc6d267f825
chengzige <2572175264@qq.com>
2022-11-20 02:03:19 +0800

    配置转发和项目端口

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8081,
+    host: '127.0.0.1',
+    proxy: {//再配置一个代理程序:
+      '/': {
+        target: `http://127.0.0.1:8080`,//后端接口地址
+        changeOrigin: true,
+        ws: false,
+        pathRewrite: {
+          '^/': ''
+        }
+      }
+    }
+  }
 })
